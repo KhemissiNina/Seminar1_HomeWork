6737760c5d0e30d00b3f6f04ad40d1e59cf70414
Nina <samninvik@mail.ru>
2022-10-16 16:15:57 +0500

    Задание 10, семинар 2

---
+++ Exercise6_Seminar2/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+ Console.WriteLine("Введите трехзначное число:");
+int number = int.Parse(Console.ReadLine());
+if (number<1000){
+    if (number>99){
+        number = number/10;
+        number = number%10;
+        Console.WriteLine(number);}
+    else 
+    Console.WriteLine("Число не трёхзначное");}
+else {
+Console.WriteLine("Число не трёхзначное");
+}
