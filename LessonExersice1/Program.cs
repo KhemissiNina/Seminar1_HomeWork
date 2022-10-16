699c25b09dec5d4476526be3303d6b3a070363e1
Nina <samninvik@mail.ru>
2022-10-16 16:38:18 +0500

    Задание1 во время семинара

---
+++ LessonExersice1/Program.cs
@@ -0,0 +1,11 @@
+﻿/*
+Проверить, делится ли первое число на второе без остатка
+*/
+Console.WriteLine("Введите первое число:");
+int FirstNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int SecondNumber = int.Parse(Console.ReadLine());
+if (SecondNumber%FirstNumber!=0)
+    Console.WriteLine($"Остаток от деления {SecondNumber} на {FirstNumber} равен: {SecondNumber%FirstNumber}");
+else
+    Console.WriteLine($"{SecondNumber} делится на {FirstNumber} без остатка");
