64437e23c3f2f1c6af7693a57769b8157902ab9f
Nina <samninvik@mail.ru>
2022-10-22 15:24:21 +0500

    Лекция3. Цикл в цикле на примере таблицы умножения

---
+++ LessonExercise7/Program.cs
@@ -0,0 +1,9 @@
+﻿// цикл в цикле на примере таблицы умножения
+for (int i=2; i<=10;i++)// внешний цикл
+{
+	for (int j=2; j<=10; j++)
+	{
+		Console.WriteLine($"{i} x {j} = {i*j}");
+	}
+	Console.WriteLine();
+}
