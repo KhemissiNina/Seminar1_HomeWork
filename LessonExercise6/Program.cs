ae3dee14e92f5b94a45e23cb30567e24dee345b2
Nina <samninvik@mail.ru>
2022-10-22 15:21:20 +0500

    Лекция3. Сравнения циклов for и while

---
+++ LessonExercise6/Program.cs
@@ -0,0 +1,24 @@
+﻿//Сравнение циклов while и for
+string Method4(int count, string text)
+{
+//int i=0;
+string result = String.Empty; // пустая строка
+/*
+while (i<count)
+{
+result=result+text;
+i++;
+}
+return result;
+}
+string res = Method4(5, "условный_текст");
+Console.WriteLine(res);
+*/
+for(int i=0; i<count; i++)
+{
+	result=result+text;
+}
+return result;
+}
+string res = Method4(5, "условный_текст");
+Console.WriteLine(res);
