261f6d09b1c35d137098650745e6aa760daf00e8
Nina <samninvik@mail.ru>
2022-10-26 22:34:33 +0500

    Семинар 3. Задача 19. Палиндром

---
+++ Seminar3Exercise19/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] NewMassiv(int number)
+{
+    int[] array = new int[5];
+    int i = 0;
+    while (i < 5)
+    {
+        array[i] = number % 10;
+        number = number / 10;
+        i++;
+    }
+    return array;
+}
+
+Console.WriteLine("Введите пятизнаячное число:");
+int number = int.Parse(Console.ReadLine());
+int[] newarray = NewMassiv(number);
+if (newarray[0] == newarray[4] && newarray[1] == newarray[3])
+    Console.WriteLine("Число является палиндромом");
+else Console.WriteLine("Число не является палиндром");
