bcf3c7b8355df548819ce4a3e0872d9a2dc7b55c
Nina <samninvik@mail.ru>
2022-10-26 22:48:42 +0500

    Семинар 3. Задача 22. Куб числа

---
+++ Seminar3Exercise22/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8,27.
+//5 -> 1, 8, 27, 64, 125
+Console.Clear();
+void Tri(int N)
+{
+    int i = 1;
+    while (i <= N)
+    {
+        int K = i * i * i;
+        Console.WriteLine($"{K} куб числа {i}");
+        i++;
+    }
+}
+Console.WriteLine("Введите число:");
+int n = int.Parse(Console.ReadLine());
+Tri(n);
