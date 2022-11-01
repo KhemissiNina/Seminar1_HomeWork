63781917bd44ef0834a7241865ab881dd06c7841
Nina <samninvik@mail.ru>
2022-11-01 20:12:04 +0500

    Семинар 4.Задание 27

---
+++ Seminar4Exercise27/Program.cs
@@ -0,0 +1,22 @@
+﻿//27.Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+Console.Clear();
+Console.Write("Введите число: ");
+string a = Console.ReadLine();
+
+int Summa (string a)
+{
+	int length=a.Length;
+	int summa=0;
+	int temp=0;
+	for (int i=0; i<length;i++)
+	{
+	temp=a[i]-'0';
+	summa+=temp;
+	}
+	return summa;
+}
+int result=Summa(a);
+Console.Write($"Сумма всех цифр в числе {a} равна {result}");
