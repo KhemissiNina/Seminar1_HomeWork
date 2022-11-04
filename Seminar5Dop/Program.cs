e0b254a7dea7a95f62d753e4754248faadb463d9
Nina <samninvik@mail.ru>
2022-11-04 16:03:47 +0500

    Семинар 5. Допольнительно

---
+++ Seminar5Dop/Program.cs
@@ -0,0 +1,21 @@
+﻿//Дополнительно
+//Дан список некоторых целых чисел, найдите значение 20 в нем и, если оно присутствует, замените его на 200. Обновите список только при первом вхождении числа 20.
+//[21, 53,20, 56, 45,20] -> [21, 53,200, 56, 45,20]
+
+List<int> Twohundred = new List<int>() { 15, 32, 39, 43, 20, 64, 17, 20 };
+
+void PrintArray(List<int> name)
+{
+    foreach (int i in Twohundred)
+    {
+        Console.Write(i + " ");
+    }
+    Console.WriteLine();
+}
+Console.Clear();
+Console.Write("Начальный  массив: ");
+PrintArray(Twohundred);
+int index = Twohundred.IndexOf(20);
+Twohundred[index] = 200;
+Console.Write("Измененный массив: ");
+PrintArray(Twohundred);
