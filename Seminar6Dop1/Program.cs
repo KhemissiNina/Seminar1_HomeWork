ef25510e2769b46f39161e12ce6c86ac3a3200e9
Nina <samninvik@mail.ru>
2022-11-07 08:25:11 +0500

    Семинар 6. дополнительно 2

---
+++ Seminar6Dop1/Program.cs
@@ -0,0 +1,17 @@
+﻿//Дано действительное положительное число a и целое неотрицательное число n. Вычислите an не используя циклы, возведение в степень через ** и функцию math.pow(), а используя рекуррентное соотношение an=a⋅an-1.
+//Решение оформите в виде функции power(a, n).
+
+int Power(int a, int n)
+{
+    if (n == 0) return 1;
+    return a * Power(a, n - 1);
+}
+Console.Clear();
+Console.Write("Введите действительное положительное число: ");
+int a = int.Parse(Console.ReadLine());
+//int a=3;
+Console.Write($"Введите целое неотрицательное число - степень в которую будем возводить число {a}: ");
+int n = int.Parse(Console.ReadLine());
+
+//int n=2;
+Console.WriteLine($"{a} в степени {n} равно {Power(a, n)}");
